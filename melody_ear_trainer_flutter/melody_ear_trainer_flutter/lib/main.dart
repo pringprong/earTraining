5021b6dac466a3bb73d56a56f3e873d9078a8acc
Monica Sleumer <pringprong@yahoo.com>
2025-05-27 21:28:30 +0800

    started new flutter app

---
+++ melody_ear_trainer_flutter/melody_ear_trainer_flutter/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+//import 'dart:async';
+//import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Text("Hello, Melody Ear Trainer!"
+      ),
+    ),
+  );
+}
+
+
