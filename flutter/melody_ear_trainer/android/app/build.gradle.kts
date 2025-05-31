95466e873a67b6a56e3ede725549d23bb2442341
Monica Sleumer <pringprong@yahoo.com>
2025-05-31 19:23:53 +0800

    added hamburger menu

--- flutter/melody_ear_trainer/android/app/build.gradle.kts
+++ flutter/melody_ear_trainer/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.melody_ear_trainer"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- flutter/melody_ear_trainer/lib/main.dart
+++ flutter/melody_ear_trainer/lib/main.dart
@@ -1,35 +1,38 @@
-import 'dart:developer' as dev;
-import 'package:flutter/foundation.dart';
+// import 'dart:developer' as dev;
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:logging/logging.dart';
+// import 'package:logging/logging.dart';
 import 'audio/audio_controller.dart';
+//import 'dart:convert';
+import 'dart:io';
 
 void main() async {
   // The `flutter_soloud` package logs everything
   // (from severe warnings to fine debug messages)
   // using the standard `package:logging`.
   // You can listen to the logs as shown below.
-  Logger.root.level = kDebugMode ? Level.FINE : Level.INFO;
-  Logger.root.onRecord.listen((record) {
-    dev.log(
-      record.message,
-      time: record.time,
-      level: record.level.value,
-      name: record.loggerName,
-      zone: record.zone,
-      error: record.error,
-      stackTrace: record.stackTrace,
-    );
-  });
+
+  // Logger.root.level = kDebugMode ? Level.FINE : Level.INFO;
+  // Logger.root.onRecord.listen((record) {
+  //   dev.log(
+  //     record.message,
+  //     time: record.time,
+  //     level: record.level.value,
+  //     name: record.loggerName,
+  //     zone: record.zone,
+  //     error: record.error,
+  //     stackTrace: record.stackTrace,
+  //   );
+  // });
 
   WidgetsFlutterBinding.ensureInitialized();
   final audioController = AudioController();
   await audioController.initialize();
-  runApp(MelodyEarTrainerApp (audioController: audioController));
+  runApp(MelodyEarTrainerApp(audioController: audioController));
 }
 
-class MelodyEarTrainerApp  extends StatelessWidget {
-  const MelodyEarTrainerApp ({required this.audioController, super.key});
+class MelodyEarTrainerApp extends StatelessWidget {
+  const MelodyEarTrainerApp({required this.audioController, super.key});
   final AudioController audioController;
 
   @override
@@ -39,8 +42,33 @@ class MelodyEarTrainerApp  extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
         useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
       home: MelodyHomePage(audioController: audioController),
+        routes: {
+          '/home': (context) => MelodyHomePage(audioController: audioController),
+          '/general': (context) => GeneralPage(),
+          // Add other routes here
+  },
+    );
+  }
+}
+
+class GeneralPage extends StatefulWidget {
+  const GeneralPage({super.key});
+
+  @override
+  State<GeneralPage> createState() => _GeneralPageState();
+}
+
+class _GeneralPageState extends State<GeneralPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('General Settings')),
+      body: Center(
+        child: Text('General settings will be implemented here.'),
+      ),
     );
   }
 }
@@ -53,65 +81,148 @@ class MelodyHomePage extends StatefulWidget {
 }
 
 class _MelodyHomePageState extends State<MelodyHomePage> {
-  static const _gap = SizedBox(height: 16);
-  bool filterApplied = false;
+  List<String> mappingKeys = [];
+  List<String> instruments = [];
+  String selectedKey = "";
+  String selectedInstrument = "";
+  int numberOfNotes = 5;
+  bool allowRepeatedNotes = false;
+  bool startWithDo = true;
+  bool endWithDo = true;
+
+  @override
+  void initState() {
+    super.initState();
+    loadMappingFiles();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Flutter SoLoud Demo')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(title: Text('Melody Ear Trainer')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: <Widget>[
-            OutlinedButton(
-              onPressed: () {
-                widget.audioController.playSound('assets/audio/C4.mp3');
-                Future.delayed(Duration(milliseconds: 800), () {
-                  widget.audioController.playSound('assets/audio/E4.mp3');
-                  Future.delayed(Duration(milliseconds: 800), () {
-                    widget.audioController.playSound('assets/audio/G4.mp3');
-                });
-               });
+            ListTile(
+              title: Text('Melody'),
+              onTap: () {
+                // Update the state of the app
+                // Then close the drawer
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/home');
               },
-              child: const Text('Play Sound'),
-            ),
-            _gap,
-            OutlinedButton(
-              onPressed: () {
-                widget.audioController.startMusic();
+            ),            ListTile(
+              title: Text('General'),
+              onTap: () {
+                // Update the state of the app
+                // Then close the drawer
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/general');
               },
-              child: const Text('Start Music'),
             ),
-            _gap,
-            OutlinedButton(
-              onPressed: () {
-                widget.audioController.fadeOutMusic();
+            ListTile(
+              title: Text('Tonic'),
+              onTap: () {
+                // Update the state of the app
+                // Then close the drawer
+                Navigator.pop(context);
               },
-              child: const Text('Fade Out Music'),
             ),
-            _gap,
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text('Apply Filter'),
-                Checkbox(
-                  value: filterApplied,
-                  onChanged: (value) {
-                    setState(() {
-                      filterApplied = value!;
-                    });
-                    if (filterApplied) {
-                      widget.audioController.applyFilter();
-                    } else {
-                      widget.audioController.removeFilter();
-                    }
-                  },
-                ),
-              ],
+            ListTile(
+              title: Text('Scales'),
+              onTap: () {
+                // Update the state of the app
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Chords'),
+              onTap: () {
+                // Update the state of the app
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Chord Sets'),
+              onTap: () {
+                // Update the state of the app
+                // Then close the drawer
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            DropdownButton<String>(
+              hint: Text('Select Key'),
+              value: selectedKey,
+              onChanged: (String? newValue) {
+                if (newValue is String) {
+                  setState(() {
+                    selectedKey = newValue;
+                  });
+                }
+              },
+              items:
+                  mappingKeys.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+            ),
+            DropdownButton<String>(
+              hint: Text('Select Instrument'),
+              value: selectedInstrument,
+              onChanged: (String? newValue) {
+                if (newValue is String) {
+                  setState(() {
+                    selectedInstrument = newValue;
+                  });
+                }
+              },
+              items:
+                  instruments.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Logic to generate melody and play
+                widget.audioController.playSound('assets/audio/C4.mp3');
+              },
+              child: Text('Play Melody'),
+            ),
+            // Additional UI elements for notes and chords can be added here
+          ],
+        ),
+      ),
     );
+  }
+
+  Future<void> loadMappingFiles() async {
+    // Load Mapping.txt and populate mappingKeys and instruments
+    String mappingData = await File('mapping/Mapping.txt').readAsString();
+    List<String> lines = mappingData.split('\n');
+    for (String line in lines) {
+      List<String> parts = line.split('\t');
+      if (parts.isNotEmpty && !mappingKeys.contains(parts[0])) {
+        mappingKeys.add(parts[0]);
+      }
+      if (parts.length > 1 && !instruments.contains(parts[1])) {
+        instruments.add(parts[1]);
+      }
+    }
+    setState(() {});
   }
 }
